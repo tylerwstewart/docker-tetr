f43e123c318457795335881741067797645f522e
chazzam <chazz.m@gmail.com>
2016-11-14 18:37:14 -0600

    updating build scripts
    now using a host volume for /home/tc/src and /home/tc/tc-deliver
    
    Supports the host src being empty, partially complete, or complete
    
    supports overwriting files in tc-deliver, and organizes all of the
    deliverables into TC-VERSION.x/TC-ARCH/tcz/ folders
    
    pulls out the packages and the bfe submission files as deliverables
    
    also includes a few bug fixes (mostly stupid mistakes on my part)
    apologies for the noise on those...
    
    very close to being ready to make a python script to manage the building...

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+HOST_DLVR="$HOME/tc-deliver"
+HOST_SRCS="$HOME/srctc"
+HOST_DIRS="$HOST_DLVR $HOST_SRCS"
 
-DOCKER_IMAGE="cmoye/tc-imager:7.2-x86-2"
-DOCKER_VOLUMES="--volumes-from tc-src -v $HOME/tc-deliver:/home/tc/tc-deliver:rw"
+DOCKER_IMAGE="chazzam/tetr:7.2-x86"
+DOCKER_VOL_DLVR="-v $HOST_DLVR:/home/tc/tc-deliver:rw"
+DOCKER_VOL_SRCS="-v $HOST_SRCS:/home/tc/src:rw"
+DOCKER_VOLUMES="$DOCKER_VOL_DLVR $DOCKER_VOL_SRCS"
 DOCKER_ENVS="-e TCMIRROR=http://pecan.digium.internal:81/tinycore-testing/"
 
 DOCKER_ARGS="$DOCKER_VOLUMES $DOCKER_ENVS $DOCKER_IMAGE"
@@ -36,23 +41,41 @@ EOF
   exit 1
 }
 
+mkdir_volume_directories() {
+  for d in $HOST_DIRS; do
+    [ -d "$d" ] || (mkdir -p "$d" && chmod -R u+rwX,g+rwX,o+rwX "$d")
+  done
+}
+
+docker_build() {
+  local build_args=""
+  build_args="$(echo $DOCKER_ENVS|sed -e 's/-e /--build-arg /g')"
+  ( cd ..;
+    docker build $build_args -t $DOCKER_IMAGE .
+  )
+}
+
 docker_shell() {
+  mkdir_volume_directories;
   docker run -it --entrypoint sh $DOCKER_ARGS $@
 }
 
 build_packages() {
+  mkdir_volume_directories;
   for pkg in $@; do
     docker run $DOCKER_ARGS tet $pkg || exerr "Error building package $pkg";
   done
 }
 
 test_extensions() {
+  mkdir_volume_directories;
   for pkg in $@; do
     docker run $DOCKER_ARGS tettest $pkg || exerr "Error testing package $pkg";
   done
 }
 
 remaster() {
+  mkdir_volume_directories;
   REMASTER=1
   docker run $DOCKER_ARGS $@ || exerr "Error bundling remastered image for $1";
 }
